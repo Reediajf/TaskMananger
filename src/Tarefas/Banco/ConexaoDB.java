ce016bc0252ce3cd75d2b1c8457b63fe44e2063c
Jaider Flávio <jaideerferreira@gmail.com>
2025-02-19 14:21:08 -0300

    Update ConexaoDB.java

--- src/Tarefas/Banco/ConexaoDB.java
+++ src/Tarefas/Banco/ConexaoDB.java
@@ -4,9 +4,9 @@ import java.sql.*;
 
 public class ConexaoDB {
 
-    private static String mysql_host = "jdbc:mysql://127.0.0.1:3306/tarefasdb"; //Host do banco de dados.
-    private  static String mysql_username = "root"; //Usuario do banco de dados.
-    private static String mysql_password = "admin"; //Senha do banco de dados.
+    private static String mysql_host = ""; //Host do banco de dados.
+    private  static String mysql_username = ""; //Usuario do banco de dados.
+    private static String mysql_password = ""; //Senha do banco de dados.
 
     public static Connection getConexao() {
         try {
